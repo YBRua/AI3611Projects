19ee05c734c1a744f791df085297bc9a56be2c3a
YBRua <ybiur0929@163.com>
2022-06-02 14:44:21 +0800

    Updated just rush in AVSC - activate environment first

--- AudioVideoSceneRecog/just_rush.sh
+++ AudioVideoSceneRecog/just_rush.sh
@@ -1,3 +1,6 @@
+source /dssg/home/acct-stu/stu491/.bashrc
+conda activate sed
+
 python train.py --config_file configs/$1.yaml
 python evaluate.py --experiment_path experiments/$1
 python eval_prediction.py --prediction ./experiments/$1/prediction.csv --label ./data/evaluate/fold1_evaluate.csv
